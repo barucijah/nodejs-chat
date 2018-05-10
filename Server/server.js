46470fa4c535d2fdc8bb8018497a4a3c8fcc8777
Haris Barucija <barucija_haris@hotmail.com>
2018-05-10 23:44:13 +0200

    'aa'

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,23 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',function (socket) {
+    console.log('new user connected');
+
+    socket.on('disconnect',function () {
+        console.log('User was disconnected');
+    });
+});
 
 app.use(express.static(publicPath));
-app.listen(port, function () {
+server.listen(port, function () {
     console.log("Server has started on port 3000");
 });
