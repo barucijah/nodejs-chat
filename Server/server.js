fd1d0d97742f3e120b99ce9ba29b49c607dd9148
Haris Barucija <barucija_haris@hotmail.com>
2018-05-11 00:31:18 +0200

    add newMessage and createMessage events

--- Server/server.js
+++ Server/server.js
@@ -12,6 +12,16 @@ var io = socketIO(server);
 io.on('connection',function (socket) {
     console.log('new user connected');
 
+    socket.emit('newMessage',{
+        from: 'mike@hotmail.com',
+        text:'hello',
+        createAt:123
+    });
+
+    socket.on('createMessage',function (message) {
+        console.log('createMessage',message);
+    });
+
     socket.on('disconnect',function () {
         console.log('User was disconnected');
     });
