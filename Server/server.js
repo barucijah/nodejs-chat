47c923ba840a875c3303028f20533ae13ddf199b
Haris Barucija <barucija_haris@hotmail.com>
2018-05-16 01:00:44 +0200

    geo1

--- Server/server.js
+++ Server/server.js
@@ -1,41 +1,39 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message')
-
-const publicPath = path.join(__dirname,'../public');
+const {generateMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connection',function (socket) {
-    console.log('new user connected');
+app.use(express.static(publicPath));
 
-    socket.emit('newMessage',generateMessage('Admin','Welcome'));
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-    socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined')
-    );
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.on('createMessage',function (message,callback) {
-        console.log('createMessage',message);
-        io.emit('newMessage',generateMessage(message.from,message.text));
-        callback('ovo je sa servera');
-        /*socket.broadcast.emit('newMessage',{
-            from:message.from,
-            text:message.text,
-            createAt:new Date().getTime()
-        });*/
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
     });
 
-    socket.on('disconnect',function () {
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`));
+    });
+
+    socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
 });
 
-app.use(express.static(publicPath));
-server.listen(port, function () {
-    console.log("Server has started on port 3000");
+server.listen(port, () => {
+    console.log(`Server is up on ${port}`);
 });
